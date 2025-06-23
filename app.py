be1d83228173ebf2fb7b293a24de2c4ce1b1d12b
Kevin Oliveros <kevin.oliveros@emapta.com>
2025-06-23 15:56:45 +0800

    first draft prototype

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -14,54 +14,53 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Task %r>' % self.id
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+    date_uploaded = db.Column(db.DateTime, default= datetime.now(timezone.utc))
+    date_modified = db.Column(db.DateTime, default= datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return '<Document %r>' % self.id
     
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
-    
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-    if request.method == 'POST':
-        task.content = request.form['content']
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-    else:
-        return render_template('update.html', task=task)
-    
-@app.route('/login')
+# PAGES    
+@app.route('/')
 def login():
     return render_template('login.html')
 
 @app.route('/login-page')
 def login_page():
     return render_template('login-page.html')
+
+@app.route('/documents', methods=['POST', 'GET'])
+def documents():
+    if request.method == 'POST':
+        file_name = request.form.get('name')
+        file_size = request.form.get('size')
+        file_type = request.form.get('type')
+
+        if not all([file_name, file_size, file_type]):
+            return 'Missing data', 400
+
+        new_document = Document(
+            name=file_name,
+            size=int(file_size),
+            type=file_type
+        )
+
+        try:
+            db.session.add(new_document)
+            db.session.commit()
+            return redirect('/documents')
+        except Exception as e:
+            return f'There was an issue adding your document: {e}', 500
+    else:
+        documents = Document.query.order_by(Document.date_uploaded.desc()).all()
+        return render_template('document-list.html', documents=documents)
 
 if __name__ == "__main__":
     app.run(debug=True)
